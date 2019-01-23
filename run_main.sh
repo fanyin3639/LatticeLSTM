630c00a1d9bb4163926482b408ae1b9e7c0bae22
wuwei <lxww301@gmail.com>
2019-01-23 19:15:01 +0800

    remove datasets

--- run_main.sh
+++ run_main.sh
@@ -1,9 +1,6 @@
-export CUDA_VISIBLE_DEVICES=1
-python main.py --status train \
-		--train data/ResumeNER/train.char.bmes \
-		--dev data/ResumeNER/dev.char.bmes \
-		--test data/ResumeNER/test.char.bmes \
-		--savemodel data/ResumeNER/saved_model \
+# export CUDA_LAUNCH_BLOCKING=1
+export CUDA_VISIBLE_DEVICES=2
+python main.py
 
 
 # python main.py --status decode \
